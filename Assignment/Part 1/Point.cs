6cb6379a6704e751a04801914f70507c1dcf8264
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-15 17:44:46 +0300

    Assignment Part 01

---
+++ Assignment/Part 1/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemnt
+{
+    struct Point
+    {
+        private readonly int X;
+        private readonly int Y;
+
+        public Point(int x, int y)
+        {
+            if (x <= 0 || y <= 0)
+            {
+                throw new ArgumentException("0 Or Less is Not Valid");
+            }
+            this.X = x; this.Y = y;
+        }
+
+        static public void DisplayDistancBetweenTwoPoints(Point p1, Point p2)
+        {
+            int dx = p2.X - p1.X;
+            int dy = p2.Y - p1.Y;
+            Console.WriteLine(Math.Round(Math.Sqrt(dx * dx + dy * dy)));
+        }
+    }
+}
---
+++ Assignment/Part 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Assignemnt;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main( )
+        {
+            #region Part 01
+            
+            #region Q1.Person Struct
+            /// Person p1 = new Person("Mena", 21);
+            /// Person p2 = new Person("Hamada", 25);
+            /// Person p3 = new Person("Any", 30);
+            /// 
+            /// Person[] people = { p1, p2, p3 };
+            /// 
+            /// foreach (Person person in people)
+            /// {
+            ///     Console.WriteLine(person);
+            /// }
+            #endregion
+
+            #region Q2.Calc Point2D Distance
+            /// Point p1 = new Point(4, 3);
+            /// Point p2 = new Point(7, 8);
+            /// 
+            /// Point.DisplayDistancBetweenTwoPoints(p1, p2);
+            #endregion
+
+            #region Q3.Person Oldest Details
+            /// Person[] people = Person.TakeUsersDetailsFromUser(3);
+            /// 
+            /// Console.WriteLine($" The Oldest Person => {Person.GetOldestUser(people)}");
+            #endregion
+            
+            #endregion
+
+
+        }
+    }
+}
